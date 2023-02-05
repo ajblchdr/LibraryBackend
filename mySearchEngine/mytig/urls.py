2928e670f1b52b22c87d9c9e59ebc646e84e921a
ajblchdr <anoujeanbalachandran@gmail.com>
2023-02-05 11:18:57 -0500

    update search and filter

--- mySearchEngine/mytig/urls.py
+++ mySearchEngine/mytig/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('books/', views.BookViewSet.as_view()),
     path('book/<int:pk>', views.BookDetailViewSet.as_view()),
     path('books/kmp/<str:txt>', views.BookSearchKMP.as_view()),
-    path('book/regex/<str:txt>', views.BookSearchRegex.as_view())
+    path('books/regex/<str:txt>', views.BookSearchRegex.as_view()),
+    path('books/lang/<str:lang>', views.BookFilterByLanguage.as_view())
 ]
